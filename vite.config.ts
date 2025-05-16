03d68fdad54075ba8a71c61bf91d49c4d3730578
zada-devel <zada.devel@gmail.com>
2025-05-17 03:15:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,5 @@ import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    https: {
-      key: fs.readFileSync('./localhost.key'),
-      cert: fs.readFileSync('./localhost.crt'),
-    },
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  plugins: [vue()]
 })
